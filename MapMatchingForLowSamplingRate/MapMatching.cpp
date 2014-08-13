1ed91365890db419c2e10276ff46cf4ac9c8a6c5
Casey Yang <knightykc@gmail.com>
2014-08-13 22:46:52 +0800

    update on Aug 13, 2014;

--- MapMatchingForLowSamplingRate/MapMatching.cpp
+++ MapMatchingForLowSamplingRate/MapMatching.cpp
@@ -5,8 +5,8 @@
 vector<string> outputFileNames;
 list<Traj*> trajList;
 string rootFilePath = "D:\\Document\\MDM Lab\\Data\\GISCUP2012_Data\\";
-string inputDirectory = "input_90";
-string outputDirectory = "output_90";
+string inputDirectory = "input_60";
+string outputDirectory = "output_60";
 Map routeNetwork = Map(rootFilePath, 1000);
 //保存计算过的两点间最短距离，键pair对表示起点和终点，值pair表示两点间最短距离和对应的deltaT
 //保存的deltaT的原因是：如果deltaT过小，则返回的最短距离可能为INF；而当再遇到相同起点和终点、而deltaT变大时，最短距离可能就不是INF了
@@ -32,11 +32,13 @@ struct Score//代表某个轨迹点对应的一个候选路段
 	long double score;//候选路段所具有的整体概率
 	int preColumnIndex;//候选路段的前序路段的列索引
 	double distLeft;//轨迹点的投影点到候选路段起点的距离
+	vector<long double> priorProbs;
 	Score(Edge* edge, long double score, int pre, double distLeft){
 		this->edge = edge;
 		this->score = score;
 		this->preColumnIndex = pre;
 		this->distLeft = distLeft;
+		priorProbs = vector<long double>();
 	}
 };
 
@@ -152,6 +154,8 @@ list<Edge*> MapMatching(list<GeoPoint*> &trajectory){
 						routeNetworkDistBetweenTwoTrajPoints = routeNetworkDistBetweenTwoEdges + currentDistLeft + formerDistToEnd;
 					}
 					long double transactionProb = exp(-fabs((long double)distBetweenTwoTrajPoints - (long double)routeNetworkDistBetweenTwoTrajPoints) / BT) / BT;//转移概率
+					//在前一条候选路段的priorProbs中保存到下一条候选路段的转移概率，作为下一条候选路段的先验概率
+					formerCanadidateEdge.priorProbs.push_back(transactionProb);
 					/*GIS2012CUP的优化加在此处，对transactionProb进行修改*/
 					long double tmpTotalProbForTransaction = formerCanadidateEdge.score * transactionProb;
 					if (currentMaxProbTmp < tmpTotalProbForTransaction){//保留当前转移概率和已知最大转移概率中较大者
@@ -186,19 +190,19 @@ list<Edge*> MapMatching(list<GeoPoint*> &trajectory){
 
 	//得到全局匹配路径
 	int startColumnIndex = GetStartColumnIndex(scoreMatrix.back());//得到最后一个轨迹点的在scoreMatrix对应行中得分最高的列索引，即全局匹配路径的终点
-	for (int i = scoreMatrix.size() - 1; i >= 0; i--){
-
-		//if (startColumnIndex != -1){
-		//	mapMatchingResult.push_front(scoreMatrix[i][startColumnIndex].edge);
-		//	startColumnIndex = scoreMatrix[i][startColumnIndex].preColumnIndex;
-		//}
-		//else
-		//{
-		//	mapMatchingResult.push_front(NULL);
-		//	if (i > 0){
-		//		startColumnIndex = GetStartColumnIndex(scoreMatrix[i - 1]);
-		//	}
-		//}
+	mapMatchingResult.push_front(scoreMatrix.back()[startColumnIndex].edge);
+	int lastColumnIndex = startColumnIndex;
+	for (int i = scoreMatrix.size() - 2; i >= 0; i--){
+		long double maxPosteriorProb = -1;
+		for (int j = 0; j < scoreMatrix[i].size(); j++){
+			long double tmpPosteriorProb = scoreMatrix[i][j].score * scoreMatrix[i][j].priorProbs[lastColumnIndex] / scoreMatrix[i + 1][lastColumnIndex].score;
+			if (tmpPosteriorProb > maxPosteriorProb){
+				maxPosteriorProb = tmpPosteriorProb;
+				startColumnIndex = j;
+			}
+		}
+		mapMatchingResult.push_front(scoreMatrix[i][startColumnIndex].edge);
+		lastColumnIndex = startColumnIndex;
 	}
 	//调试代码：输出最终的概率矩阵：如果有某个轨迹点的所有候选路段的整体概率均为均为无穷小/无穷大，那可能就不正常，需要进一步检查该行概率的得到过程
 	//for (int i = 0; i < scoreMatrix.size(); i++){
@@ -223,7 +227,7 @@ void main(){
 	cout << "开始地图匹配！" << endl;
 	for (list<Traj*>::iterator trajIter = trajList.begin(); trajIter != trajList.end(); trajIter++){
 		list<Edge*> resultList = MapMatching(*(*trajIter));
-		ofstream MatchedEdgeOutput(rootFilePath +outputDirectory+ "\\" + outputFileNames[trajIndex]);
+		ofstream MatchedEdgeOutput(rootFilePath + outputDirectory + "\\" + outputFileNames[trajIndex]);
 		Traj::iterator trajPointIter = (*trajIter)->begin();
 		for (list<Edge*>::iterator edgeIter = resultList.begin(); edgeIter != resultList.end(); edgeIter++, trajPointIter++){
 			if (*edgeIter != NULL){
