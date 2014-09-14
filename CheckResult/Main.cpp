b66db904ccd5c2a94a2599dbf76c40b13b1a8902
Casey Yang <knightykc@gmail.com>
2014-09-14 11:44:52 +0800

    update project CheckResult;

--- CheckResult/Main.cpp
+++ CheckResult/Main.cpp
@@ -6,6 +6,10 @@
 #include<list>
 using namespace std;
 
+string rootFilePath = "D:\\MapMatchingProject\\Data\\新加坡数据\\";
+string outputDirectory = "day7\\day7_splited_input";//轨迹文件所在文件夹路径。其中包含的轨迹文件名要求：以“input_”开头
+string answerDirectory = "day7\\day7_splited_120s_input";//降低采样率后的轨迹文件所在文件夹路径。
+string resultFileName = "null";//检查结果输出文件名
 list<string> fileNameList = list<string>();//保存文件名集合
 list<list<int>> matchedResultList = list<list<int>>();//保存匹配结果集合
 list<list<int>> matchedAnswerList = list<list<int>>();//保存答案集合
@@ -26,7 +30,7 @@ list<int> ReadOneResultFile(string filePath){
 }
 
 //读入所有匹配结果文件和相应的答案文件
-void ReadFolder(string folderDir, string outputDirectory, string answerDirectory)
+void ReadFolder()
 {
 	/*文件目录结构为
 	* folderDir
@@ -35,7 +39,7 @@ void ReadFolder(string folderDir, string outputDirectory, string answerDirectory
 	* |-answerDirectory
 	*   |-output_000011.txt ...
 	*/
-	string completeInputFilesPath = folderDir + outputDirectory + "\\" + "*.txt";
+	string completeInputFilesPath = rootFilePath + outputDirectory + "\\" + "*.txt";
 	const char* dir = completeInputFilesPath.c_str();
 	_finddata_t fileInfo;//文件信息
 	long lf;//文件句柄
@@ -45,11 +49,11 @@ void ReadFolder(string folderDir, string outputDirectory, string answerDirectory
 	else {
 		do {
 			string inputFileName = fileInfo.name;
-			matchedResultList.push_back(ReadOneResultFile(folderDir + outputDirectory + "\\" + inputFileName));
+			matchedResultList.push_back(ReadOneResultFile(rootFilePath + outputDirectory + "\\" + inputFileName));
 			string answerFileName = inputFileName.substr(6, inputFileName.size() - 10);
 			fileNameList.push_back(answerFileName);
 			answerFileName = "output_" + answerFileName + ".txt";
-			matchedAnswerList.push_back(ReadOneResultFile(folderDir + answerDirectory + "\\" + inputFileName));
+			matchedAnswerList.push_back(ReadOneResultFile(rootFilePath + answerDirectory + "\\" + inputFileName));
 		} while (_findnext(lf, &fileInfo) == 0);
 		_findclose(lf);
 		return;
@@ -63,7 +67,10 @@ void Check(){
 	list<list<int>>::iterator resultIter = matchedResultList.begin();
 	list<list<int>>::iterator answerIter = matchedAnswerList.begin();
 	list<string>::iterator fileNameIter = fileNameList.begin();
-	ofstream filePathFout = ofstream("files_sr_60.txt");
+	ofstream filePathFout;
+	if (resultFileName != "null"){
+		filePathFout = ofstream("files_sr_60.txt");
+	}
 	for (; resultIter != matchedResultList.end(); resultIter++, answerIter++, fileNameIter++){
 		int wrongCount = 0;
 		list<int>::iterator resultEdgeIter = resultIter->begin();
@@ -75,7 +82,7 @@ void Check(){
 		}
 		double tmpRate = (wrongCount + 0.0) / resultIter->size();
 		//只输出匹配错误率大于0.2的轨迹文件名
-		if (tmpRate > 0.2){
+		if (resultFileName != "null"&&tmpRate > 0.2){
 			filePathFout << (*fileNameIter) << " " << tmpRate << endl;
 		}
 		averageErrorRate += tmpRate;
@@ -85,9 +92,25 @@ void Check(){
 	cout << "平均错误率：" << averageErrorRate << endl;
 }
 
-int main(){
-	ReadFolder("D:\\MapMatchingProject\\Data\\新加坡数据\\15days\\", "15days_output2", "15days_answer");
-	Check();
+int main(int argc, char* argv[]){
+	if (argc != 1 && argc != 3 && argc != 4){
+		cout << "至多应该有三个参数：第一个为匹配结果文件所在文件夹路径，第二个为答案文件所在文件夹路径，第三个为检查结果输出文件名。" << endl;
+	}
+	else{
+		if (argc == 3){
+			outputDirectory = argv[1];
+			answerDirectory = argv[2];
+		}
+		if (argc == 4){
+			outputDirectory = argv[1];
+			answerDirectory = argv[2];
+			resultFileName = argv[3];
+		}
+		cout << "匹配结果文件所在文件夹路径：" << outputDirectory << endl;
+		cout << "答案文件所在文件夹路径：" << answerDirectory << endl;
+		ReadFolder();
+		Check();
+	}
 	system("pause");
 	return 0;
 }
