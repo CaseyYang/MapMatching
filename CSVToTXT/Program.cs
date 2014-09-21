1f376c247f6ffa47cc58ab08940ac54dc7ffe724
Casey Yang <knightykc@gmail.com>
2014-09-21 16:02:07 +0800

    update;

---
+++ CSVToTXT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVToTXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            string csvFIlePath="";
+            StreamReader fileReader = new StreamReader(csvFIlePath);
+            while (!fileReader.EndOfStream)
+            {
+                string rawStr=fileReader.ReadLine();
+                string[] strs = rawStr.Split(',');
+
+            }
+        }
+    }
+}
